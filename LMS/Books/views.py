e923a4c6092e088dfd79984af163a44f968ee865
jawadahmedkh <jawadfarman786@gmail.com>
2025-06-26 00:23:47 +0500

    .

--- LMS/Books/views.py
+++ LMS/Books/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render,redirect
+from .models import Book,Category
+from django.contrib import messages
 
 def add_Book(request):
     if request.method == 'POST':
-        data = request.POST.get()
+        data = request.POST
+        title = data.get('title')
+        author = data.get('author')
+        isbn = data.get('isbn')
+        quantity = data.get('quantity')
+        available_count = data.get('quantity')
+        category = data.get('category')
+
+        if Book.objects.filter(isbn=isbn).exists():
+            messages.error(request, "Book Already Exists!")
+            return redirect('/')
+        
+        category_ = Category.objects.create(name=category)
+        Book.objects.create(
+                    title = title,
+                    author = author,
+                    isbn = isbn,
+                    quantity = quantity,
+                    available_count = available_count ,
+                    category =  category_
+        )
+        messages.info(request,f'Book "{title}" added Successfully!')
+        return redirect('/')
+    
+    return render(request,'add_book.html')
 
 def delete_book(request):
     pass
